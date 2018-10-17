2b981575c627519609c87edf76f4d1fe775c3e3a
vinayg1 <vinayg1@nationwide.com>
2018-10-17 16:10:00 -0400

    First commit

---
+++ src/main/java/first/aws/investments/InvestmentsApplication.java
@@ -0,0 +1,12 @@
+package first.aws.investments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentsApplication.class, args);
+	}
+}
---
+++ src/main/java/first/aws/investments/controller/InputController.java
@@ -0,0 +1,17 @@
+package first.aws.investments.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class InputController {
+
+
+	@RequestMapping(value = "/investments")
+	@ResponseBody
+	public String getInvestments() {
+		return "hello investments";
+	}
+}
